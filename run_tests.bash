8f7429540e6e7be3b5f7d1ad44187ef5b41d0c77
JP Sullivan <jonpsull@cisco.com>
2018-07-25 12:26:44 +0100

    Update to python 3.7, tracking docker image

--- run_tests.bash
+++ run_tests.bash
@@ -3,6 +3,14 @@
 # Copyright (c) 2018 Cisco Systems, Inc.
 # All rights reserved.
 
+export PATH=${PATH}:~/.local/bin
 pip install -q --user -r test-requirements.txt
 
-$HOME/.local/bin/tox --workdir /tmp
+TOXENV=flake8
+for version in $(sed -n 's/[[:space:]]*- python:\(.*\)/\1/pg' .travis.yml | tr -d ' ')
+do
+    [[ -s "$(which python$version)" ]] && \
+        TOXENV=${TOXENV}${TOXENV:+,}py$(echo $version | tr -d '.')
+done
+
+TOXENV=$TOXENV $HOME/.local/bin/tox --workdir /tmp
